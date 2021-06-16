8b0d9aab1ebd364c935015eaf9d251a20aac7c7f
sethgabrielbrady <sethgabrielbrady@gmail.com>
2021-06-15 23:38:09 -0400

    Adds first commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+
 
 createApp(App).use(store).use(router).mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>ThreeJS playground for testing components.</h1>
+    <a href="https://threejs.org/">ThreeJs.org</a>
   </div>
 </template>
+
+<script>
+
+</script>
